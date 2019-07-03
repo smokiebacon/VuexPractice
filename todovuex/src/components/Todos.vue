7da1750280909e46201fe0018299f888c4dff480
Abraham Hsu <smokiebacon@gmail.com>
2019-07-03 09:34:09 -0700

    Abraham adds Pagination with BootstrapVue

--- todovuex/src/components/Todos.vue
+++ todovuex/src/components/Todos.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
+  <div class="overflow-auto">
     <h3>To Do:</h3>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination>
 
+    <p class="mt-3">Current Page: {{ currentPage }}</p>
+    <b-table id="my-table" :items="items" :per-page="perPage" :current-page="currentPage" small></b-table>
     <div class="todos">
       <div v-for="todo in allTodos" :key="todo.id" class="todo">
         {{todo.title}}
@@ -23,14 +31,31 @@ export default {
   name: "Todos",
   data() {
     return {
-      hover: false
+      perPage: 3,
+      currentPage: 1,
+      items: [
+        { id: 1, first_name: "Fred", last_name: "Flintstone" },
+        { id: 2, first_name: "Wilma", last_name: "Flintstone" },
+        { id: 3, first_name: "Barney", last_name: "Rubble" },
+        { id: 4, first_name: "Betty", last_name: "Rubble" },
+        { id: 5, first_name: "Pebbles", last_name: "Flintstone" },
+        { id: 6, first_name: "Bamm Bamm", last_name: "Rubble" },
+        { id: 7, first_name: "The Great", last_name: "Gazzoo" },
+        { id: 8, first_name: "Rockhead", last_name: "Slate" },
+        { id: 9, first_name: "Pearl", last_name: "Slaghoople" }
+      ]
     };
   },
   methods: {
     ...mapActions(["fetchTodos", "deleteTodo"])
   }, //we use spread operator here so we can keep adding on methods later
   //mapActions is exactly what it sounds like, get the
-  computed: mapGetters(["allTodos"]), //array because allTodos in an array of objects
+  computed: {
+    ...mapGetters(["allTodos"]), //array because allTodos in an array of objects
+    rows() {
+      return this.items.length;
+    }
+  },
 
   //created is the same as ComponentDidMount lifecyle method in React
   created() {
